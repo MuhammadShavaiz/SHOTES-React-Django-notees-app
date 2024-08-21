66acc704d6bccd1c2b455a99b263447fbceb044f
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-08-21 11:55:40 -0700

    initial commit

--- Notes Django/shotes/api/urls.py
+++ Notes Django/shotes/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getRoutes, name='routes')   
+    path('',views.getRoutes, name='routes'),
+    path('notes/',views.getNotes, name='Get Notes'),
+    path('notes/<str:pk>',views.getNote,name='Get Note'),   
 ]
--- Notes Django/shotes/api/views.py
+++ Notes Django/shotes/api/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import NoteSerializer
+from .models import Note
 # Create your views here.
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('our api', safe=False)
+    routes=[{
+        'Endpoint':'/notes/',
+        'method': 'GET',
+        'body':None,
+        'description':'creation of note'
+    },
+    {
+        'Endpoint':'/notes/id/',
+        'method':'GET',
+        'body':None,
+        'description':'returns a single note'
+    },
+    {
+        'Endpoint':'/notes/create/',
+        'method':'POST',
+        'body':{'body':""},
+        'description':'creates a single note'
+    },
+    {
+        'Endpoint':'/notes/id/update/',
+        'method':'PUT',
+        'body':{'body':""},
+        'description':'update notes from sent data'
+    },
+    {
+        'Endpoint':'/notes/id/delete/',
+        'method':'DELETE',
+        'body':None,
+        'description':'deletes an existing note'
+    }]
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes,many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes,many = False)
+    return Response(serializer.data)
