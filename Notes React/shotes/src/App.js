d2ea54fc465f96ba466f1a3f9fabd4c1867d2425
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-08-21 13:17:16 -0700

    layout complete

---
+++ Notes React/shotes/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import './App.css';
+import NoteListPage from './pages/NoteListPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <NoteListPage/>
+    </div>
+  );
+}
+
+export default App;
