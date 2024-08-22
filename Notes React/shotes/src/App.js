b6709dc654e495594ab1bebe9896f3bc8dd90522
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-08-22 12:42:06 -0700

    added styling

--- Notes React/shotes/src/App.js
+++ Notes React/shotes/src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import NoteListPage from './pages/NoteListPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotePage from './pages/NotePage';
+import { useState} from 'react';
+
 
 function App() {
+  const [theme, setTheme] = useState('dark');
   return (
     <Router>
-      <div className="App">
-        <Header />
+      <div className={`container ${theme}`}>
+        <div className='app'>
+        <Header theme={theme} setTheme={setTheme}/>
         <Routes>
           <Route path="/" element={<NoteListPage />} />
           <Route path="/note/:id" element={<NotePage />} />
         </Routes>
+        </div>
       </div>
     </Router>
   );
--- Notes React/shotes/src/components/Header.js
+++ Notes React/shotes/src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {ReactComponent as ThemeIcon} from '../assets/idea-svgrepo-com.svg'
 
-const Header = () => {
+const Header = ({theme,setTheme}) => {
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? '' : 'dark'));
+  };
   return (
-    <div>
-      <h1>Note Header</h1>
+    <div className='app-header'>
+      <h1>Note List</h1>
+      <ThemeIcon onClick={toggleTheme} style={{width:"30px",height:'30px'}}/>
     </div>
   )
 }
