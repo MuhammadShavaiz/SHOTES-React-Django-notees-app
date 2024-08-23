24ce48fd512778b4371455ef0a89041bc84bee98
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-08-23 07:33:44 -0700

    added update option

--- Shotes Frontend/src/pages/NotePage.js
+++ Shotes Frontend/src/pages/NotePage.js
@@ -1,31 +1,52 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import {ReactComponent as ArrowLeft} from '../assets/left-arrow-svgrepo-com.svg'
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowLeft } from '../assets/left-arrow-svgrepo-com.svg';
 
 const NotePage = () => {
-    const {id} = useParams()
-const [note, setNote] = useState(null)
-useEffect(() =>{
-    const getNote = async() => {
-        const response = await fetch(`/api/notes/${id}`)
-        const data = await response.json()
-        setNote(data)
-    }
-    getNote()
-},[id])
-  return (
-    <div className='note'>
-        <div className="note-header">
-            <h3>
-                <Link to='/'>
-                <ArrowLeft/>
-                </Link>
-            </h3>
-        </div>
-      <textarea defaultValue={note?.body}></textarea>
-    </div>
-  )
-}
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [note, setNote] = useState(null);
 
-export default NotePage
+    useEffect(() => {
+        const getNote = async () => {
+            const response = await fetch(`/api/notes/${id}`);
+            const data = await response.json();
+            setNote(data);
+        };
+        getNote();
+    }, [id]);
+
+    const updateNote = async () => {
+            await fetch(`/api/notes/${id}/update/`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(note),
+            });
+    };
+
+    const submitUpdate = () => {
+        if (note) {
+            updateNote().then(() => {
+                navigate('/');
+            });
+        }
+    };
+
+    return (
+        <div className='note'>
+            <div className="note-header">
+                <h3>
+                    <ArrowLeft onClick={submitUpdate} />
+                </h3>
+            </div>
+            <textarea
+                onChange={(e) => setNote({ ...note, body: e.target.value })}
+                value={note?.body}
+            ></textarea>
+        </div>
+    );
+};
+
+export default NotePage;
